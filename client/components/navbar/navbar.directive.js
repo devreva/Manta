d5acff9cc7447d67d0a159436fa636aea0a1b245
makitake2013 <makitake2013@gmail.com>
2015-12-05 16:23:44 +0900

    プロジェクトのテンプレート作成

---
+++ client/components/navbar/navbar.directive.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('mantaApp')
+  .directive('navbar', () => ({
+    templateUrl: 'components/navbar/navbar.html',
+    restrict: 'E',
+    controller: 'NavbarController',
+    controllerAs: 'nav'
+  }));
