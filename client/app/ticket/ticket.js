360ab0f5cd3415c8654ee56a53fd38a9bbe81632
devreva <devreva@gmail.com>
2015-12-13 17:12:19 +0900

    チケット登録

--- client/app/ticket/ticket.js
+++ client/app/ticket/ticket.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('mantaApp')
-  .config(function ($stateProvider) {
+  .config(function($stateProvider) {
     $stateProvider
       .state('ticket', {
         url: '/ticket',
