4e38d697b290ba7a53e165b6efa539a9310b502d
devreva <devreva@gmail.com>
2015-12-19 15:15:24 +0900

    剛のミスを兄がカバーして競合を解決

--- server/routes.js
+++ server/routes.js
@@ -10,11 +10,8 @@ import path from 'path';
 module.exports = function(app) {
 
   // Insert routes below
-<<<<<<< HEAD
   app.use('/api/accounts', require('./api/account'));
-=======
   app.use('/api/tickets', require('./api/ticket'));
->>>>>>> 360ab0f5cd3415c8654ee56a53fd38a9bbe81632
   app.use('/api/things', require('./api/thing'));
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
