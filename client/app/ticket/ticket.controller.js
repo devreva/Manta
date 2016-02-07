09850a8872ff79117b01aa1032832c174ded1e85
devreva <devreva@gmail.com>
2016-02-07 17:57:01 +0900

    登録したタイトルをかんばん上で編集可能にする クリックでテキストボックス表示に切り替え、マウスオーバーで更新処理する

--- client/app/ticket/ticket.controller.js
+++ client/app/ticket/ticket.controller.js
@@ -5,12 +5,12 @@ angular.module('mantaApp')
   .controller('TicketCtrl', function($scope, $http, socket) {
     $scope.statusNames = ['Todo', 'Next', 'Now', 'Finished'];
     $http.get('/api/tickets').success(function(tickets) {
-      $scope.showTickets(tickets);
+      $scope.createDataSet4Show(tickets);
       $scope.syncTickets = tickets;
-      socket.syncUpdates('ticket', $scope.syncTickets, $scope.showTickets);
+      socket.syncUpdates('ticket', $scope.syncTickets, $scope.createDataSet4Show);
     })
 
-    $scope.showTickets = function(tickets) {
+    $scope.createDataSet4Show = function(tickets) {
       $scope.tickets = [];
       tickets.forEach(function(ticket) {
         if (!$scope.tickets[ticket.status]) {
@@ -29,6 +29,15 @@ angular.module('mantaApp')
           $scope.ticket.title = '';
         });
       }
+    };
+
+    $scope.updateTicket = function() {
+      if (this.item._id && this.item.isEditing) {
+        $http.put('/api/tickets/' + this.item._id, {
+          title: this.item.title
+        });
+      }
+      this.item.isEditing = false;
     };
 
     $scope.onDrop = function(event, ui, status) {
